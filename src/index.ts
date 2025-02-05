5de5df470eb63b465429bef1e3c8ee67c11ec467
Carlos V <carlosv.provoos@gmail.com>
2025-02-05 17:23:52 -0500

    initial config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import type { Plugin } from "@elizaos/core";
-import { startAnyone } from "./actions/startAnyone.ts";
-import { stopAnyone } from "./actions/stopAnyone.ts";
+import { startAnyone } from "./actions/startAnyone";
+import { stopAnyone } from "./actions/stopAnyone";
 export * as actions from "./actions";
 
-export const anyonePlugin: Plugin = {
+export const anyonePlugin = {
     name: "anyone",
     description: "Proxy requests through Anyone",
     actions: [startAnyone, stopAnyone],
