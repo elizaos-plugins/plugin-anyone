2f4dd72d6bd67c0155ffc85b325f737e6d2da3a6
Avaer Kazmer <ultimus@gmail.com>
2025-02-04 23:45:30 -0800

    Build

--- dist/index.js
+++ dist/index.js
@@ -101,7 +101,7 @@ var startAnyone = {
     const proxyService = AnyoneProxyService.getInstance();
     await proxyService.initialize();
     _callback({
-      text: `Started Anyone`
+      text: "Started Anyone"
     });
     return true;
   },
@@ -174,7 +174,7 @@ var stopAnyone = {
     proxyService.cleanup();
     await AnyoneClientService.stop();
     _callback({
-      text: `Stopped Anyone and cleaned up proxy`
+      text: "Stopped Anyone and cleaned up proxy"
     });
     return true;
   },
